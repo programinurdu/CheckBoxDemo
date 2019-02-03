a61a5a4a017add46a0e4f023a46ddbac7e9e28d4
programinurdu <aasdb@progd23.com>
2019-02-03 13:43:35 +0000

    Add project files.

---
+++ CheckBoxDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using CheckBoxDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheckBoxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewRecord()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult NewRecord(StudentModel studentModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ CheckBoxDemo/Models/StudentModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CheckBoxDemo.Models
+{
+    public class StudentModel
+    {
+        
+        public int StudentId { get; set; }
+
+        [Display(Name = "Full Name:")]
+        [Required(ErrorMessage = "Full Name is required.")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email:")]
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Hob Moor Nusery")]
+        public bool HobMoorNusery { get; set; }
+
+        [Display(Name = "Green Lane Nusery")]
+        public bool GreenLaneNusery { get; set; }
+
+        [Display(Name = "Nuneaton Nusery")]
+        public bool NuneatonNusery { get; set; }
+
+    }
+}
